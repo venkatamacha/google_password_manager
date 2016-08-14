060b36e1a6f75675613fd6051c0cae165090157f
Venkata Macha <venkatamacha@college.harvard.edu>
2016-08-14 15:11:51 -0500

    Initialized skeleton of application

---
+++ google_password_manager/google_password_manager/SettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsViewController.swift
+//  google_password_manager
+//
+//  Created by Venkata Macha on 8/14/16.
+//  Copyright © 2016 Venkata Macha. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
